9c05668b6471e5deb58072440379466ba1b5500c
tadashdev <henriqueteste0f@gmail.com>
2025-03-28 13:13:48 -0300

    Update autelog.py

--- autelog.py
+++ autelog.py
@@ -53,7 +53,7 @@ pttwin_headers = {
     "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/134.0.0.0'
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/134.0.0.0"
 }
 
 api_info = {
